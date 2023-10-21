ed1d88bdf35122659f2c24e29825719001783409
Gerardo Duran-Martin <g.duran@me.com>
2023-10-21 09:21:11 +0000

    feat: low_rank_filter.py

--- rebayes_mini/methods/low_rank_filter.py
+++ rebayes_mini/methods/low_rank_filter.py
@@ -12,6 +12,7 @@ class LoFiState:
     diagonal: chex.Array
     low_rank: chex.Array
 
+
 class ExpfamFilter(kf.ExpfamFilter):
     def __init__(
         self, apply_fn, log_partition, suff_statistic, dynamics_covariance,
@@ -35,17 +36,18 @@ class ExpfamFilter(kf.ExpfamFilter):
         )
     
     def _predict(self, state):
+        I_lr = jnp.eye(self.rank)
         mean_pred = state.mean
         diag_pred = 1 / (1 / state.diagonal + self.dynamics_covariance)
 
         C = jnp.einsum("ji,j,j,jk->ik",
-            state.low_rank, diag_pred, 1 / self.diagonal, state.low_rank.T
+            state.low_rank, diag_pred, 1 / state.diagonal, state.low_rank
         )
-        C = jnp.linalg.inv(C)
+        C = jnp.linalg.inv(I_lr + self.dynamics_covariance * C)
 
         low_rank_pred = jnp.einsum(
             "i,i,ij,jk->ik",
-            diag_pred, 1 / self.diagonal, state.low_rank, C
+            diag_pred, 1 / state.diagonal, state.low_rank, C
         )
 
         state_pred = state.replace(
@@ -55,21 +57,80 @@ class ExpfamFilter(kf.ExpfamFilter):
         )
         return state_pred
 
-    def _update(self, state_pred, x, y):
-        ...
+    
+    def _update_dlr(self, low_rank_hat):
+        singular_vectors, singular_values, _ = jnp.linalg.svd(low_rank_hat, full_matrices=False)
+
+        singular_vectors_drop = singular_vectors[:, self.rank:] # Ut
+        singular_values_drop = singular_values[self.rank:] # Λt
+
+        # Update new low rank
+        singular_vectors = singular_vectors[:, :self.rank] # Ut
+        singular_values = singular_values[:self.rank] # Λt
+        low_rank_new = jnp.einsum("Dd,d->Dd", singular_vectors, singular_values)
+
+        # Obtain additive term for diagonal
+        lr_drop = jnp.einsum("Dd,d->Dd", singular_vectors_drop, singular_values_drop)
+        diag_drop = jnp.einsum("ij,ij->i", lr_drop, lr_drop)
+
+        return low_rank_new, diag_drop
+                
+
+    @partial(jax.jit, static_argnums=(0,))
+    def _update(self, bel_pred, x, y):
+        eta = self.link_fn(bel_pred.mean, x).astype(float)
+        yhat = self.mean(eta)
+        yobs = self.suff_statistic(y)
+        Rt = jnp.atleast_2d(self.covariance(eta))
+        Ht = self.grad_link_fn(bel_pred.mean, x)
+
+        At = jnp.linalg.inv(jnp.linalg.cholesky(Rt))
+        low_rank_hat = jnp.c_[bel_pred.low_rank, Ht.T @ At.T]
+        Gt = jnp.linalg.inv(
+            jnp.eye(self.rank + 1) + low_rank_hat.T @ low_rank_hat / self.dynamics_covariance
+        )
+        Ct = Ht.T @ At.T @ At
+
+        # Kalman gain
+        K1 = jnp.einsum("i,ij->ij", 1 / bel_pred.diagonal, Ct)
+        K2 = jnp.einsum(
+            "i,ij,jk,lk,l,lm->im",
+            1 / bel_pred.diagonal, low_rank_hat, Gt,
+            low_rank_hat, 1 / bel_pred.diagonal, Ct
+        )
+        Kt = K1 - K2
+        
+        mean_new = bel_pred.mean + Kt @ (yobs - yhat)
+        low_rank_new, diag_drop = self._update_dlr(low_rank_hat)
+        diag_new = bel_pred.diagonal + diag_drop
+
+        bel_new = bel_pred.replace(
+            mean=mean_new,
+            diagonal=diag_new,
+            low_rank=low_rank_new,
+        )
+        return bel_new
+
 
     def step(self, bel, xs, callback_fn):
         xt, yt = xs
         bel_pred = self._predict(bel)
+        bel_update = self._update(bel_pred, xt, yt)
 
-        eta = self.link_fn(bel_pred.mean, xt).astype(float)
-        yhat = self.mean(eta)
-        yobs = self.suff_statistic(yt)
-        Rt = self.covariance(eta)
-        Ht = self.grad_link_fn(bel_pred.mean, xt)
+        output = callback_fn(bel_update, bel_pred, xt, yt)
+        return bel_update, output
 
-        At = jnp.linalg.inv(jnp.linalg.cholesky(Rt))
-        low_rank_hat = jnp.c_[bel_pred, Ht.T @ At.T]
-        Gt = jnp.linalg.inv(
-            jnp.eye(self.rank + 1) + low_rank_hat.T @ low_rank_hat / self.dynamics_covariance
+
+class BernoulliFilter(ExpfamFilter):
+    def __init__(self, apply_fn, dynamics_covariance):
+        super().__init__(
+            apply_fn, self._log_partition, self._suff_stat, dynamics_covariance
         )
+
+    @partial(jax.jit, static_argnums=(0,))
+    def _log_partition(self, eta):
+        return jnp.log1p(jnp.exp(eta)).sum()
+
+    @partial(jax.jit, static_argnums=(0,))
+    def _suff_stat(self, y):
+        return y
