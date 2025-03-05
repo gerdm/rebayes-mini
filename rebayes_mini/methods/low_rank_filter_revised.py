a5735d65b42f70b77512167d919c0c068f52aeb8
Gerardo Duran-Martin <g.duran@me.com>
2025-03-05 13:28:35 +0000

    refactor: low_rank_filter_revised

--- rebayes_mini/methods/low_rank_filter_revised.py
+++ rebayes_mini/methods/low_rank_filter_revised.py
@@ -10,7 +10,6 @@ class LowRankState:
     """State of the Low Rank Filter"""
     mean: chex.Array
     low_rank: chex.Array
-    diagonal: chex.Array
 
 def orthogonal(key, n, m):
     """
@@ -34,9 +33,9 @@ class LowRankCovarianceFilter(BaseFilter):
 
     def _init_low_rank(self, key, nparams, cov, diag):
         if diag:
-            loading_hidden = jnp.fill_diagonal(jnp.zeros((self.rank, nparams)), jnp.ones(nparams), inplace=False)
+            loading_hidden = cov * jnp.fill_diagonal(jnp.zeros((self.rank, nparams)), jnp.ones(nparams), inplace=False)
         else:
-            loading_hidden = orthogonal(key, self.rank, nparams)
+            loading_hidden = cov * orthogonal(key, self.rank, nparams)
 
         return loading_hidden
 
@@ -44,12 +43,10 @@ class LowRankCovarianceFilter(BaseFilter):
         """
         TODO: Double check!!
         """
-        key_full, key_sub = jax.random.split(key)
         dim_full = len(bel.mean)
         shape = shape if shape is not None else (1,)
         shape_sub = (*shape, self.rank)
-        shape_full = (*shape, dim_full)
-        eps = jax.random.normal(key_sub, shape_sub)
+        eps = jax.random.normal(key, shape_sub)
 
         # params = jnp.einsum("ji,sj->si", bel.low_rank, eps) + eps_full * jnp.sqrt(self.dynamics_covariance) + bel.mean
         params = jnp.einsum("ji,sj->si", bel.low_rank, eps) + bel.mean
@@ -67,12 +64,9 @@ class LowRankCovarianceFilter(BaseFilter):
         nparams = len(init_params)
         low_rank = self._init_low_rank(key, nparams, cov, low_rank_diag)
 
-        diag = jnp.zeros(nparams) * cov
-
         return LowRankState(
             mean=init_params,
             low_rank=low_rank,
-            diagonal=diag
         )
 
     def project(self, *matrices):
@@ -96,7 +90,6 @@ class LowRankCovarianceFilter(BaseFilter):
         bel_pred = bel.replace(
             mean=mean_pred,
             low_rank=low_rank_pred,
-            diagonal=bel.diagonal + self.dynamics_covariance
         )
         return bel_pred
     
@@ -106,13 +99,13 @@ class LowRankCovarianceFilter(BaseFilter):
         Ht = self.grad_mean_fn(bel.mean, x)
         W = bel.low_rank
 
-        # C = jnp.r_[W @ Ht.T, jnp.sqrt(self.dynamics_covariance) * Ht.T, Rt_half]
-        C = jnp.r_[W @ Ht.T, Rt_half]
+        C = jnp.r_[W @ Ht.T, jnp.sqrt(self.dynamics_covariance) * Ht.T, Rt_half]
+        # C = jnp.r_[W @ Ht.T, Rt_half]
         S_half = jnp.linalg.qr(C, mode="r") # Squared-root of innovation
 
         # transposed Kalman gain and innovation
         Mt = jnp.linalg.solve(S_half, jnp.linalg.solve(S_half.T, Ht))
-        Kt_T = Mt @ W.T @ W + Mt * bel.diagonal
+        Kt_T = Mt @ W.T @ W + Mt * self.dynamics_covariance
         err = y - yhat
         return Kt_T, err, Rt_half, Ht
     
@@ -128,187 +121,3 @@ class LowRankCovarianceFilter(BaseFilter):
             low_rank=low_rank_update
         )
         return bel
-
-
-class ExpfamFilter(kf.ExpfamFilter):
-    def __init__(
-        self, apply_fn, log_partition, suff_statistic, dynamics_covariance, rank
-    ):
-        """
-        Moment-matched Low-rank Extended Kalman filter
-
-        Parameters
-        ----------
-        apply_fn: function
-            Conditional expectation for a measurement
-        log_partition: function
-            [to be deprecated]
-        suff_statistic: function
-            Sufficient statistic given an observation
-        dynamics_covariance: float
-            Additive dynamics covariance to correct for model misspecification
-        rank: int
-            Dimension of low-rank component
-        """
-        super().__init__(apply_fn, log_partition, suff_statistic, dynamics_covariance)
-        self.rank = rank
-
-    def init_bel(self, params, cov=1.0):
-        self.rfn, self.link_fn, init_params = self._initialise_link_fn(self.apply_fn, params)
-        self.grad_link_fn = jax.jacrev(self.link_fn)
-        nparams = len(init_params)
-
-        low_rank = jnp.fill_diagonal(jnp.zeros((self.rank, nparams)), jnp.ones(nparams), inplace=False)
-        diag = jnp.ones(nparams) * cov
-
-        return LowRankState(
-            mean=init_params,
-            low_rank=low_rank,
-            diagonal=diag
-        )
-
-    def project(self, *matrices):
-        """
-        Create rank-d matrix P such that
-        P^T P approx A + B
-        """
-        Z = jnp.vstack(matrices)
-        ZZ = jnp.einsum("ij,kj->ik", Z, Z)
-        singular_vectors, singular_values, _ = jnp.linalg.svd(ZZ, hermitian=True, full_matrices=False)
-        singular_values = jnp.sqrt(singular_values) # square root of eigenvalues
-
-        P = jnp.einsum("i,ji,jk->ik", 1 / singular_values, singular_vectors, Z)
-        P = jnp.einsum("d,dD->dD", singular_values[:self.rank], P[:self.rank])
-        return P
-    
-    def mean_fn(self, bel, x):
-        return self.link_fn(bel, x)
-
-    def sample_params(self, key, bel, shape=None):
-        """
-        TODO: Double check!!
-        """
-        key_full, key_sub = jax.random.split(key)
-        dim_full = len(bel.mean)
-        shape = shape if shape is not None else (1,)
-        shape_sub = (*shape, self.rank)
-        eps = jax.random.normal(key_sub, shape_sub)
-
-        params = jnp.einsum("ji,sj->si", bel.low_rank, eps) + bel.mean
-        return params
-
-    def sample_fn(self, key, bel):
-        params = self.sample_params(key, bel).squeeze()
-        def fn(x): return self.mean_fn(params, x).squeeze()
-        return fn
-
-    def predict(self, state):
-        mean_pred = state.mean
-        low_rank_pred = state.low_rank
-
-        state_pred = state.replace(
-            mean=mean_pred,
-            low_rank=low_rank_pred,
-            diagonal=state.diagonal + self.dynamics_covariance
-        )
-
-        return state_pred
-    
-    def _innovation_and_gain(self, state, y, x):
-        eta = self.link_fn(state.mean, x).astype(float)
-        yhat = self.mean(eta)
-        yobs = self.suff_statistic(y)
-        Rt_half = jnp.linalg.cholesky(jnp.atleast_2d(self.covariance(eta)), upper=True)
-        Ht = self.grad_link_fn(state.mean, x)
-        W = state.low_rank
-
-        # C = jnp.r_[W @ Ht.T, jnp.sqrt(self.dynamics_covariance) * Ht.T, Rt_half]
-        C = jnp.r_[W @ Ht.T, Rt_half]
-        S_half = jnp.linalg.qr(C, mode="r") # Squared-root of innovation
-
-        # transposed Kalman gain and innovation
-        Mt = jnp.linalg.solve(S_half, jnp.linalg.solve(S_half.T, Ht))
-        Kt_T = Mt @ W.T @ W + Mt * state.diagonal
-        err = y - yhat
-        return Kt_T, err, Rt_half, Ht
-    
-    def update(self, state, y, x):
-        Kt_T, err, Rt_half, Ht = self._innovation_and_gain(state, y, x)
-        mean_update = state.mean + jnp.einsum("ij,i->j", Kt_T, err)
-        low_rank_update = self.project(
-            state.low_rank - state.low_rank @ Ht.T @ Kt_T, Rt_half @ Kt_T
-        )
-
-        state = state.replace(
-            mean=mean_update,
-            low_rank=low_rank_update
-        )
-        return state
-
-    def step(self, bel, y, x, callback_fn):
-        bel_pred = self.predict(bel)
-        bel_update = self.update(bel_pred, y, x) 
-        output = callback_fn(bel_update, bel_pred, y, x)
-        return bel_update, output
-
-
-class BernoulliFilter(ExpfamFilter):
-    def __init__(self, apply_fn, dynamics_covariance, rank, ):
-        super().__init__(
-            apply_fn, self._log_partition, self._suff_stat,
-            dynamics_covariance, rank, 
-        )
-
-    @partial(jax.jit, static_argnums=(0,))
-    def _log_partition(self, eta):
-        return jnp.log1p(jnp.exp(eta)).sum()
-
-    @partial(jax.jit, static_argnums=(0,))
-    def _suff_stat(self, y):
-        return y
-
-
-class MultinomialFilter(ExpfamFilter):
-    def __init__(self, apply_fn, dynamics_covariance, rank, eps=0.1, ):
-        super().__init__(
-            apply_fn, self._log_partition, self._suff_stat,
-            dynamics_covariance, rank, 
-        )
-        self.eps = eps
-
-    @partial(jax.jit, static_argnums=(0,))
-    def _log_partition(self, eta):
-        eta = jnp.append(eta, 0.0)
-        Z =  jax.nn.logsumexp(eta)
-        return Z.sum()
-
-    @partial(jax.jit, static_argnums=(0,))
-    def _suff_stat(self, y):
-        return y
-
-    def mean(self, eta):
-        return jax.nn.softmax(eta)
-
-    def covariance(self, eta):
-        mean = self.mean(eta)
-        return jnp.diag(mean) - jnp.outer(mean, mean) + jnp.eye(len(eta)) * self.eps
-
-
-class GaussianFilter(ExpfamFilter):
-    def __init__(self, apply_fn, dynamics_covariance, rank, variance=1.0, ):
-        super().__init__(
-            apply_fn, self._log_partition, self._suff_stat, dynamics_covariance, rank, 
-        )
-        self.variance = variance
-
-    def mean(self, eta):
-        return eta
-
-    def covariance(self, eta):
-        return self.variance * jnp.eye(1)
-
-    def _suff_stat(self, y):
-        return y
-
-    def _log_partition(self, eta):
-        return (eta ** 2 / 2).sum()
